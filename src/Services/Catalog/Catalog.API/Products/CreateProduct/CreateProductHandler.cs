d44aa8826085fb19ae03e0289796ace4aa7f6db9
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2025-04-21 22:38:47 +0300

    Catalog.API with VSA and CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+
+internal class CreateProductCommandHandler
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        Product product = new()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // TODO
+        // save to database
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
