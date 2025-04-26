f18a7331b36cecca8f05caa3680581cfcca39881
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2025-04-26 22:42:44 +0300

    Ordering Microservice Layer with DDD and Clean Architecture

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddCarter();
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        //app.MapCarter();
+
+        return app;
+    }
+}
