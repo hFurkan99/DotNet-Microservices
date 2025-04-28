64544c1d33ab6208235adf1506e02ab46e340df0
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2025-04-28 23:26:32 +0300

    Ordering.Application Layer with CQRS and MediatR

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -12,6 +12,8 @@ public static class DependencyInjection
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
         return services;
